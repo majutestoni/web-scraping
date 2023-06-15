565e9bffc68af1fa93e653d6e1564bcc5b839af2
majutestoni <majuliatestoni@gmail.com>
2023-06-15 10:29:58 -0300

    [add] arquivo .env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,14 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { CreateNoticia1686766033270 } from "./migrations/1686766033270-createNoticia"
 import Noticia from "../app/entities/Noticia"
+require('dotenv/config');
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    username: "root",
-    password: "root",
-    database: "noticia",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [Noticia],
